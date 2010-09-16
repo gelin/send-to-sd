4f7239cf5330fa3b37560cc3d5c0f64af3a1cb7d
Denis Nelubin <dnelubin@gmail.com>
2010-09-16 23:21:16 +0700

    Updated icon. Switched to version 0.3.

--- src/ru/gelin/android/sendtosd/IntentUtils.java
+++ src/ru/gelin/android/sendtosd/IntentUtils.java
@@ -10,7 +10,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
-import android.os.Bundle;
 import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.util.Log;
