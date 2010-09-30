745b4f1d74391a6c88a38ec1eb6b3e42af3d95fc
Denis Nelubin <dnelubin@gmail.com>
2010-10-01 00:23:51 +0700

    Added dispatched intent to handle both SEND and SEND_MULTIPLE.

---
+++ src/ru/gelin/android/sendtosd/SendDispatcherActivity.java
@@ -0,0 +1,27 @@
+package ru.gelin.android.sendtosd;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class SendDispatcherActivity extends Activity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent(); 
+        String action = intent.getAction();
+        if (Intent.ACTION_SEND.equals(action)) {
+            intent.setClass(this, SendActivity.class);
+            startActivity(intent);
+        } else if (Intent.ACTION_SEND_MULTIPLE.equals(action)) {
+            intent.setClass(this, SendMultipleActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, R.string.unsupported_intent, Toast.LENGTH_LONG).show();
+        }
+        finish();
+    }
+
+}
