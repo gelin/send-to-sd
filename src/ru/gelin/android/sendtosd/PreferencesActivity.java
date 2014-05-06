c9e54a09f7133a302f108bb0fe455947a1dbcc7e
Denis Nelubin <dnelubin@gmail.com>
2014-05-06 22:57:31 +0700

    Updated manifest.

--- src/ru/gelin/android/sendtosd/PreferencesActivity.java
+++ src/ru/gelin/android/sendtosd/PreferencesActivity.java
@@ -1,7 +1,6 @@
 package ru.gelin.android.sendtosd;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.preference.PreferenceActivity;
 
 /**
@@ -9,8 +8,6 @@ import android.preference.PreferenceActivity;
  */
 public class PreferencesActivity extends PreferenceActivity {
 
-	Handler handler = new Handler();
-	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
